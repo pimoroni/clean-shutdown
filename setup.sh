b4a97c948cd87067436549d32c7b25eb80864287
RogueM <chaosgen6@googlemail.com>
2017-05-16 16:05:05 +0100

    explicit override of all parameters for all supported products

--- setup.sh
+++ setup.sh
@@ -239,14 +239,25 @@ if [ "$PRODUCT" == "onoffshim" ]; then
     config_set trigger_pin 17
     config_set poweroff_pin 4
     config_set led_pin 17
-    config_set hold_time 2
+    config_set hold_time 1
+    config_set shutdown_delay=5
+    config_set polling_rate=2
 elif [ "$PRODUCT" == "zerolipo" ]; then
     echo -e "\nApplying default settings for OnOff SHIM..."
     config_set trigger_pin 4
+    config_set poweroff_pin off
+    config_set led_pin off
+    config_set hold_time 0
     config_set shutdown_delay=5
+    config_set polling_rate=2
 elif [ "$PRODUCT" == "phatbeat" ]; then
     echo -e "\nApplying default settings for OnOff SHIM..."
     config_set trigger_pin 12
+    config_set poweroff_pin off
+    config_set led_pin off
+    config_set hold_time 0
+    config_set shutdown_delay=5
+    config_set polling_rate=2
 else
     if [ "$FORCE" != '-y' ]; then
         echo
