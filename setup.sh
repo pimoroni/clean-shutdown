75fce92ba4a611c455a23268131249e75f47837e
RogueM <chaosgen6@googlemail.com>
2017-05-16 19:21:14 +0100

    couple of messages corrections

--- setup.sh
+++ setup.sh
@@ -261,7 +261,7 @@ elif [ "$PRODUCT" == "phatbeat" ]; then
     config_set shutdown_delay 0
     config_set polling_rate 2
 elif [ "$PRODUCT" == "default" ]; then
-    echo -e "\nApplying default settings for pHAT BEAT..."
+    echo -e "\nApplying default settings..."
     config_set trigger_pin 4
     config_set poweroff_pin off
     config_set led_pin off
